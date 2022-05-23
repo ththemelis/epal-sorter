666004b946e1ce5f542bdd57a9c1c00e9ad178c8
Theo Themelis <themelis@sch.gr>
2022-05-23 10:53:14 +0300

    Update epal-sorter.ino

--- epal-sorter.ino
+++ epal-sorter.ino
@@ -1,121 +1,144 @@
-#include <AccelStepper.h>
+#include <AccelStepper.h>   // Βιβλιοθήκη για τον χειρισμό του βηματικού κινητήρα
+#include <Servo.h>    // Βιβλιοθήκη για τον χειρισμό του σερβοκινητήρα
+#include <ArduinoQueue.h>
 
-// Motor pin definitions:
-#define motorPin1  8      // IN1 on the ULN2003 driver
-#define motorPin2  9      // IN2 on the ULN2003 driver
-#define motorPin3  10     // IN3 on the ULN2003 driver
-#define motorPin4  11     // IN4 on the ULN2003 driver
+ArduinoQueue<int> queue(3);
 
-// Define the AccelStepper interface type; 4 wire motor in half step mode:
+// Ακροδέκτες σύνδεσης του κινητήρα
+#define motorPin1  8      // IN1
+#define motorPin2  9      // IN2
+#define motorPin3  10     // IN3
+#define motorPin4  11     // IN4
+
+// Ο χειρισμός του κινητήρα θα γίνεται σε half step mode
 #define MotorInterfaceType 8
 
-// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper library with 28BYJ-48 stepper motor:
 AccelStepper stepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin3, motorPin2, motorPin4);
+Servo servoSorter;
 
+// Ακροδέκτες σύνδεση του αισθητήρα χρώματος
 #define S0 3
 #define S1 4
 #define S2 6
 #define S3 5
 #define sensorOut 7
 
-// Stores frequency read by the photodiodes
-int redFrequency = 0;
-int greenFrequency = 0;
-int blueFrequency = 0;
+// Αποθήκευση των τιμών για κάθε ένα βασικό χρώμα, από τον αισθητήρα
+int red = 0;
+int green  = 0;
+int blue = 0;
 
-int redColor = 0;
-int greenColor = 0;
-int blueColor = 0;
+int servoPositions[]= {50,80,110,140,165};
+int servoPos;
+int lastPos=0;
+int lastValue;
 
 void setup() {
   Serial.begin(9600);
   
-  // Setting the outputs
+  // Ορισμός των ακροδεκτών του αισθητήρα χρώματος ως έξόδους
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   
-  // Setting the sensorOut as an input
+  // Ορισμός του ακροδέκτη του αισθητήρα χρώματος ως είσοδο
   pinMode(sensorOut, INPUT);
   
-  // Setting frequency scaling to 20%
   digitalWrite(S0,HIGH);
   digitalWrite(S1,LOW);
 
   stepper.setMaxSpeed(1000);
+  servoSorter.attach(2);
+  servoSorter.write(90);
 }
 
 void loop() {
-  // Set the current position to 0:
   stepper.setCurrentPosition(0);
 
-  // Run the motor forward at 500 steps/second until the motor reaches 4076 steps (1 revolution):
   while (stepper.currentPosition() != 512) { 
     stepper.setSpeed(500);
     stepper.runSpeed();
   }
-  delay(2000);
+  delay(200);
   
-  // Setting RED (R) filtered photodiodes to be read
+  // Προετοιμασία για ανάγνωση κόκκινου χρώματος
   digitalWrite(S2,LOW);
   digitalWrite(S3,LOW);
   
-  // Reading the output frequency
-  redFrequency = pulseIn(sensorOut, LOW);
-  
-   // Printing the RED (R) value
-  Serial.print("R = ");
-  Serial.print(redFrequency);
+  red = pulseIn(sensorOut, LOW);  // Ανάγνωση κόκκινου χρώματος
   delay(100);
-  
-  // Setting GREEN (G) filtered photodiodes to be read
+    
+  // Προετοιμασία για ανάγνωση πράσινου χρώματος
   digitalWrite(S2,HIGH);
   digitalWrite(S3,HIGH);
   
-  // Reading the output frequency
-  greenFrequency = pulseIn(sensorOut, LOW);
-  
-  // Printing the GREEN (G) value  
-  Serial.print(" G = ");
-  Serial.print(greenFrequency);
+  green  = pulseIn(sensorOut, LOW); // Ανάγνωση πράσινου χρώματος
   delay(100);
- 
-  // Setting BLUE (B) filtered photodiodes to be read
+  
+  // Προετοιμασία για ανάγνωση μπλε χρώματος
   digitalWrite(S2,LOW);
   digitalWrite(S3,HIGH);
   
-  // Reading the output frequency
-  blueFrequency = pulseIn(sensorOut, LOW);
-  
-  // Printing the BLUE (B) value 
-  Serial.print(" B = ");
-  Serial.println(blueFrequency);
-  delay(100);  
+  blue = pulseIn(sensorOut, LOW); // Ανάγνωση μπλε χρώματος
+  delay(100);
+    
+   // Εκτύπωση των τιμών των χρωμάτων που διάβασε ο αισθητήρας
+  Serial.print("R= ");
+  Serial.print(red);
+  Serial.print(" G= ");
+  Serial.print(green );
+  Serial.print(" B= ");
+  Serial.print(blue);
+  Serial.print("  ");
 
-  if ((redFrequency >= 50 && redFrequency <= 80) && greenFrequency >= 105) {
-    Serial.println("Red");
+  if ((red >= 25 && red <= 50) && (green  >= 75 && green  <= 95) && (blue >= 40 && blue <= 60)) {
+    Serial.print("Red");
+    servoPos = servoPositions[0];
   }
-
-  if ((redFrequency >= 50 && redFrequency <= 80) && (greenFrequency >= 80 && greenFrequency <= 110)) {
-    Serial.println("Orange");
+  else if ((red >= 25 && red <= 50) && green  <= 45) {
+    Serial.print("Yellow");
+    servoPos = servoPositions[0];
   }
-
-  if ((redFrequency >= 45 && redFrequency <= 65) && (greenFrequency <= 90 && blueFrequency <= 60)) {
-    Serial.println("Yellow");
+  else if ((red >= 25 && red <= 50) && green  <= 70) {
+    Serial.print("Orange");
+    servoPos = servoPositions[0];
   }
-
-  if ((redFrequency >= 105 && redFrequency <= 125) && (greenFrequency >= 95 && greenFrequency <= 135) && (blueFrequency >= 55 && blueFrequency <= 80)) {
-    Serial.println("Brown");
+  else if ((red >= 70 && red <= 95)  && green  >= 75 && blue >= 40 ) {
+    Serial.print("Brown");
+    servoPos = servoPositions[2];
   }
-
-  if ((redFrequency >= 75 && redFrequency <= 95) && (greenFrequency >= 60 && greenFrequency <= 80) && (blueFrequency >= 45 && blueFrequency <= 60)) {
-    Serial.println("Green");
+  else if ((red >= 55 && red <= 70) && (green  >= 45 && green  <= 60)) {
+    Serial.print("Green");
+    servoPos = servoPositions[3];
   }
-
-  if ((redFrequency >= 105 && redFrequency <= 125) && (greenFrequency >= 75 && greenFrequency <= 95) && (blueFrequency >= 35 && blueFrequency <= 55)) {
-    Serial.println("Blue");
+  else if ((red >= 70 && red <= 95) && (green  >= 50 && green  <= 65) && blue <= 35) {
+    Serial.print("Blue");
+    servoPos = servoPositions[4];
   }
-
+  Serial.println("");
   delay(1000);
+
+  queue.enqueue(servoPos);
+  if (queue.isFull()) {
+    lastValue = queue.dequeue();     
+    if (lastPos < lastValue) {
+      for (int i=lastPos; i<=lastValue; i++) {
+        servoSorter.write(i);
+        delay(15);
+      }
+    }
+  else {
+    if (lastValue==0) {
+      return;
+    }
+    else {
+    for (int i=lastPos; i>=lastValue; i--) {
+        servoSorter.write(i);
+        delay(15);
+    }
+   }
+  }
+  }
+  lastPos = lastValue;
 }
